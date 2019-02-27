9ac91c251070ee39770b834a0c882b5516c749a3
Neil Johnson <realdealneil@gmail.com>
2019-02-27 14:19:06 -0700

    Adding initial files.  Not much done yet

---
+++ build.sh
@@ -0,0 +1,1 @@
+g++ splineTest1.cpp -I/usr/local/include/eigen3 -o splineTest1
---
+++ splineTest1.cpp
@@ -0,0 +1,17 @@
+/******
+ * Make a spline using Eigen spline library (unsupported stuff)
+ * 
+ */
+
+#include <iostream>
+#include <cstdio>
+#include <unsupported/Eigen/Splines>
+
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+	cout << "Starting Spline Program\n";
+	return 0;
+}
