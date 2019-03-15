5daa9b5ae4e974cb83b1357a387ae6ecb85ede29
mcsauder <mcsauder@gmail.com>
2019-03-15 16:09:23 -0600

    Add Functor class back to SplineIntegroation.h.

--- catkin_ws/src/rdr_spline_path/include/spline/SplineIntegration.h
+++ catkin_ws/src/rdr_spline_path/include/spline/SplineIntegration.h
@@ -1,6 +1,6 @@
 /**
- * @file SplineIntegration.h
- * @copyright Mark Sauder
+ * @file SplineMath.h
+ * @copyright Grey Point Corporation
  */
 
 #pragma once
@@ -10,55 +10,82 @@
 #include <unsupported/Eigen/Splines>
 #include <unsupported/Eigen/NumericalIntegration>
 
-/**
- * Define the scalar typedef: e.g. float, double, int, etc.
- */
-typedef float Scalar;
-
 
 /**
- * @class SplineIntegration Helper class to simplify the usage of the
- *        Eigen::Numerical Integration class with splines.
- *
- * Example usage:
- *    Eigen::Spline3d spline {};
- *    Scalar spline_length = SplineIntegration<PathType::Dimension>::Integrate(spline, 0, 1, integrator);
+ * The integrand which is evaluated in the calculation of the arc length of a curve.
  */
 template <int dimension>
-class SplineIntegration
+class SplineFunctor
 {
 public:
-    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+    SplineFunctor(const Eigen::Spline<Scalar, dimension>& spline)
+        : spline(spline)
+    {
+    }
 
-    static Scalar Integrate(const Eigen::Spline<Scalar, dimension>& spline,
-                            const Scalar lower_limit = static_cast<Scalar>(0),
-                            const Scalar upper_limit = static_cast<Scalar>(1),
-                            Eigen::Integrator<Scalar>& integrator);
-
+    /**
+     * @param param The parameter at which to evaluate the integrand.
+     * @returns the value of the integrand at @a param.
+     */
     Scalar operator()(const Scalar param) const
     {
-        // Return the normalized derivatives.
-        return sqrt(spline.derivatives(param, 1).col(1).squaredNorm());
+        Eigen::Matrix<Scalar, dimension, 1> derivatives = spline.derivatives(param, 1).col(1);
+        return  sqrt(derivatives.squaredNorm());
     }
 
 private:
-
-    Scalar _absolute_error{1.e-3};
-    Scalar _relative_rrror{1.e-3};
+    /**
+     * @brief The spline to evaluate the arc length of.
+     */
+    const Eigen::Spline<Scalar, dimension> spline;
 
 };
 
+
 template <int dimension>
-Scalar SplineIntegration<dimension>::Integrate(const Eigen::Spline<Scalar, dimension>& spline, 
-                                               const Scalar lower_limit = static_cast<Scalar>(0),
-                                               const Scalar upper_limit = static_cast<Scalar>(1),
+class SplineMath
+{
+public:
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+   
+    static Scalar arcLength(const Eigen::Spline<Scalar, dimension>& spline,
+                            Eigen::Integrator<Scalar>& integrator);
+ 
+    static Scalar arcLength(const Eigen::Spline<Scalar, dimension>& spline,
+                            Scalar lowerLimit,
+                            Scalar upperLimit,
+                            Eigen::Integrator<Scalar>& integrator);
+
+private:
+    static Scalar arcLengthIntegrand(const Eigen::Spline<Scalar, dimension>& spline,
+                                     const Scalar& u);
+
+    static Eigen::Spline<Scalar, dimension> spline;
+};
+
+template <int dimension>
+inline Scalar SplineMath<dimension>::arcLength(const Eigen::Spline<Scalar, dimension>& spline,
                                                Eigen::Integrator<Scalar>& integrator)
 {
-    Scalar integral = integrator.quadratureAdaptive(spline,
-                                                    lower_limit,
-                                                    upper_limit,
-                                                    _absolute_error,
-                                                    _relative_rrror,
+    return SplineMath<dimension>::arcLength(spline, 0., 1., integrator);
+}
+
+template <int dimension>
+Scalar SplineMath<dimension>::arcLength(const Eigen::Spline<Scalar, dimension>& spline, 
+                                        Scalar lowerLimit,
+                                        Scalar upperLimit, 
+                                        Eigen::Integrator<Scalar>& integrator)
+{
+    Scalar absolute_error = 1.e-3;
+    Scalar relative_error = 1.e-3;
+
+    SplineFunctor<dimension> splineFunctor(spline);
+
+    Scalar integral = integrator.quadratureAdaptive(splineFunctor,
+                                                    lowerLimit,
+                                                    upperLimit,
+                                                    absolute_error,
+                                                    relative_error,
                                                     Eigen::Integrator<Scalar>::GaussKronrod15);
     
     return integral;
