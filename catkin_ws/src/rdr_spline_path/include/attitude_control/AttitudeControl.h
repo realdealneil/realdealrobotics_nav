1aa4f747a30bf94575349bb4e4eec673fb1e3071
mcsauder <mcsauder@gmail.com>
2019-03-16 19:33:04 -0600

    Added functionality for finding the closest param and calculating the desired acceleration vector.

--- catkin_ws/src/rdr_spline_path/include/attitude_control/AttitudeControl.h
+++ catkin_ws/src/rdr_spline_path/include/attitude_control/AttitudeControl.h
@@ -25,116 +25,149 @@ static constexpr double MIN_SPEED{0.};          // m/s
 class AttitudeControl
 {
 public:
-	AttitudeControl() {}
-	
-	void SetSpline(const Eigen::Spline3d& s) 
-	{
-		spline_ = s;
-	}
-	
-	void SetSplineLength(double l) 
-	{ 
-		spline_length_ = l; 
-		std::cout << "Spline length is: " << spline_length_ << "\n";
-	}	
-
-	/**
-     * \brief Find closest spot on spline nearby previous position.  
-	 * \param spline: the spline
-	 * \param prevU: previous nearest u (we assume that we're making forward progress on the spline)
-	 * \param currentPosition: position of vehicle for which we want to find the u parameter
-	 * \param currentU: output 
-	 * \param splinePosition: Corresponding 3d location on the spline 
-	 */
-	bool findNearestPointOnSpline(const Eigen::Spline3d& spline, 
-		                          const double& prevU,
-                                  const Eigen::Vector3d currentPosition, 
-		                          double& currentU,
-                                  Eigen::Vector3d& splinePosition)
-	{
-		 //! Find the position at the previous U.  That's where we'll start searching:
-		 Eigen::Vector3d prevSplinePosition = spline(prevU);
-		 
-		 //! Start with a rough search along the spline.  Look 1/5 of the spline ahead, with an increment close to 1 m:
-		 double meter_increment = 1.0;
-		 double meter_search_dist = spline_length_/5.0;
-		 double u_increment = meter_increment/spline_length_;
-		 double u_search_dist = meter_search_dist/spline_length_;
-	}	
-
-    void calculateSplineDerivatives(const double& parameter_u)
+    AttitudeControl() {}
+    
+    void SetSpline(const Eigen::Spline3d& newSpline) 
     {
-        Eigen::MatrixXd derivatives = spline_.derivatives<SPLINE_DEGREE>(parameter_u);
-
-        _tangent_unit_vector = derivatives.col(1);
-        _tangent_normal_vector = derivatives.col(2);
+        spline = newSpline;
     }
+    
+    void SetSplineLength(const double& length) 
+    { 
+        splineLength = length; 
+        std::cout << "Spline length is: " << splineLength << "\n";
+    }   
 
-	/**
-     * \brief get curvature and max tangent speed from a spline.
+    /**
+     * \brief Find closest spot on spline nearby previous position.
+     * \param currentPosition: position of vehicle for which we want to find the u parameter
+     * \return Returns the current closest parameter on the spline to the vehicle.
      */
-	bool calculateMaximumTangentSpeed(const double& parameter_u,
-		                              double& curvature,
-                                      double& max_tangent_speed)
-	{
-        calculateSplineDerivatives(parameter_u);
-
-		// Ensure perpendicularity to tangent vector.
-		_centripetal_accel_vector = _tangent_unit_vector.cross(_tangent_normal_vector.cross(_tangent_unit_vector));
-		
-		max_tangent_speed = MAX_SPEED;
-
-		if (_centripetal_accel_vector.norm() > 0)
-		{
-			_centripetal_accel_vector.normalize();
-
-			// k = |y' x y"| / ||y'^3||
-			curvature = _tangent_unit_vector.cross(_tangent_normal_vector).norm()
-							   / pow(_tangent_unit_vector.norm(), 3.0);
-
-			// a = v^2 / r   =>   v^2 * k   =>  v = sqrt(a / k)
-			max_tangent_speed = sqrt(_max_accel * curvature);
-			return true;
-		}
-
-		assert(false);
-		return false;
-	}
-	
-	/*
-
-    void calculateTangentialAccelVector(const double& parameter_u)
+    double findClosestParam(const Eigen::Vector3d currentPosition)
     {
-        double accel_magnitude = 0.;
+        // Perform a rough search for closest spline parameter at given resolution to avoid local minimum.
+        constexpr size_t discretePointsToCheck = 1000;
+        constexpr double searchIncrement = 1. / static_cast<double>(discretePointsToCheck);
+        constexpr double searchThreshold = 1e-4;
 
-        _tangenial_accel_vector = 
-    }
+        double distance = 0.;
+        double startParam = previousClosestParam;
+        double minimumDistance = (currentPosition - (*this)(startParam)).norm();
 
-    Eigen::Vector3d calculateInertialAccel(const double& parameter_u)
-    {
-        Vector3d max_velocity = calculate_maximum_tangent_speed(parameter_u);
-        calculate_tangential_accel_vector(parameter_u);
-        
-        _inertial_accel_vector = _centripetal_accel_vector + _inertial_accel_vector + _gravity_vector;
-
-        if (inertial_accel_vector.norm() == 0.)
+        for (unsigned int i = 1; i < discretePointsToCheck; ++i)
         {
-            inertial_accel_vector = Vector3d(0., 0., 0.0000001);
+            startParam += searchIncrement;
+            distance = (currentPosition - (*this)(startParam)).norm();
+            
+            if (distance < minimumDistance)
+            {
+                minimumDistance = distance;
+            }
         }
 
-        return inertial_accel_vector;
+        // Gradient search
+        double closestParam = previousClosestParam;
+        size_t counter = 0;
+
+        while (counter < discretePointsToCheck)
+        {
+            Eigen::MatrixXd derivatives = spline.derivatives<SPLINE_DEGREE>(closestParam);
+
+            Eigen::Vector3d vectorToSpline = derivatives.col(0).matrix() - currentPosition;
+
+            double numerator = tangentVector.dot(vectorToSpline);
+            double denominator = (derivatives.col(2).matrix().dot(vectorToSpline) + std::pow(tangentVector.norm(), 2.));
+
+            closestParam = std::max(std::min(previousClosestParam - numerator / denominator, 1.), 0.);
+
+            if (((closestParam - previousClosestParam) * tangentVector).norm() < searchThreshold)
+            {
+                previousClosestParam = closestParam;
+                return closestParam;
+            }
+
+            counter++;
+        }
+
+        return -1;
+    }   
+
+    Eigen::Vector3d calculateSplineDerivatives(const double& parameterU, const int derivativeDegree)
+    {
+
+        if (derivativeDegree > SPLINE_DEGREE)
+        {
+            return Eigen::Vector3d::Zero();
+        }
+    
+        Eigen::MatrixXd derivatives = spline.derivatives<SPLINE_DEGREE>(parameterU);
+        return derivatives.col(derivativeDegree);
     }
-    */
+
+    /**
+     * \brief Compute the maximum tangential speed at one point on the spline.
+     * \param parameterU The point of interest on the spline.
+     * \param curvature The computed curvature at that point on the spline.
+     */
+    double calculateMaxTangentialSpeed(const double& parameterU)
+    {
+        tangentVector = calculateSplineDerivatives(parameterU, 1);
+        tangentNormalVector = calculateSplineDerivatives(parameterU, 2);
+
+        // Ensure perpendicularity to tangent vector.
+        centripetalAccelVector = tangentVector.cross(tangentNormalVector.cross(tangentVector));
+        
+        max_tangent_speed = MAX_SPEED;
+
+        if (centripetalAccelVector.norm() > 0)
+        {
+            centripetalAccelVector.normalize();
+
+            // k = |y' x y"| / ||y'^3||
+            curvature = tangentVector.cross(tangentNormalVector).norm()
+                               / pow(tangentVector.norm(), 3.0);
+
+            // a = v^2 / r   =>   v^2 * k   =>  v = sqrt(a / k)
+            max_tangent_speed = sqrt(_max_accel * curvature);
+            return max_tangent_speed;
+        }
+
+        assert(false);
+        return 0.;
+    }
+
+    Eigen::Vector3d calculateDesiredAccelVector(const double& parameterU,
+                                                const double currentSpeed)
+    {
+        double accel_magnitude = 0.;
+        double speedError = calculateMaxTangentialSpeed(parameterU) - currentSpeed;
+
+        double centripetalAccelMagnitude = (centripetalAccelVector + gravityVector).norm();
+        double maxTangentialAccelMagnitude = MAX_ACCEL - centripetalAccelMagnitude;
+        double tangentialAccelEffort = std::min((tangentAccelerationGain * speedError), maxTangentialAccelMagnitude)
+
+        tangentialAccelVector *= tangentialAccelEffort;
+        inertialAccelVector = centripetalAccelVector + tangentialAccelVector + gravityVector;
+
+        if (inertialAccelVector.norm() == 0.)
+        {
+            inertialAccelVector = Vector3d(0., 0., 0.0000001);
+        }
+
+        return inertialAccelVector;
+    }
 
 private:
-	Eigen::Spline3d spline_;
-    Eigen::Vector3d _centripetal_accel_vector{Eigen::Vector3d::Zero()};
-    Eigen::Vector3d _inertial_accel_vector{Eigen::Vector3d::Zero()};
-    Eigen::Vector3d _tangent_accel_vector{Eigen::Vector3d::Zero()};
-    Eigen::Vector3d _tangent_unit_vector{Eigen::Vector3d::Zero()};
-    Eigen::Vector3d _tangent_normal_vector{Eigen::Vector3d::Zero()};
+    Eigen::Spline3d spline;
+    Eigen::Vector3d centripetalAccelVector{Eigen::Vector3d::Zero()};
+    Eigen::Vector3d inertialAccelVector{Eigen::Vector3d::Zero()};
+    Eigen::Vector3d tangentialAccelVector{Eigen::Vector3d::Zero()};
+    Eigen::Vector3d tangentVector{Eigen::Vector3d::Zero()};
+    Eigen::Vector3d tangentNormalVector{Eigen::Vector3d::Zero()};
     
-    Eigen::Vector3d _gravity_vector{Eigen::Vector3d(0., 0., -GRAVITY)};
+    Eigen::Vector3d gravityVector{Eigen::Vector3d(0., 0., -GRAVITY)};
     
-    double spline_length_ = 25.0;	//! Set this to minimize search speeds. 
+    double tangentAccelerationGain{1.}; // Proportional gain to tilt the trust vector.
+    double previousClosestParam{0.};    // Previous closest spline parameter.
+    double splineLength{25.0};          //! Set this to minimize search speeds.
 };
