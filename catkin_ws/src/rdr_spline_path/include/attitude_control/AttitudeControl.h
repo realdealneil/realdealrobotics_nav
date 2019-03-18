b0deeec1900d50340752b940bf4b0d758b79080f
mcsauder <mcsauder@gmail.com>
2019-03-18 17:20:11 -0600

    Does not compile.  Intermediate commit for remote access.

--- catkin_ws/src/rdr_spline_path/include/attitude_control/AttitudeControl.h
+++ catkin_ws/src/rdr_spline_path/include/attitude_control/AttitudeControl.h
@@ -26,19 +26,34 @@ class AttitudeControl
 {
 public:
     AttitudeControl() {}
-    
-    void SetSpline(const Eigen::Spline3d& newSpline) 
+
+    /**
+     * \brief Sets the spline.
+     * \newSpline The new spline to be set.
+     */
+    void setSpline(const Eigen::Spline3d& newSpline) 
     {
         spline = newSpline;
     }
-    
-    void SetSplineLength(const double& length) 
+
+    /**
+     * \brief Sets the length of the current spline.
+     * \length The length to be set.
+     */
+    void setSplineLength(const double& length) 
     { 
         splineLength = length; 
         std::cout << "Spline length is: " << splineLength << "\n";
     }   
-    
-    double getCurvature(void) { return curvature; }
+
+    /**
+     * \brief Sets the tangential acceleration gain value.
+     * \gain The new gain to be set.
+     */
+    void setTangentAccelerationGain(const double& gain)
+    {
+        tangentAccelerationGain = gain;
+    }
 
     /**
      * \brief Find closest spot on spline nearby previous position.
@@ -53,7 +68,7 @@ public:
 
         double distance = 0.;
         double startParam = previousClosestParam;
-        
+
         double minimumDistance = (currentPosition - Eigen::Vector3d(spline(startParam))).norm();
 
         // Search the entire spline for closest spline parameter at given resolution to find the global minimum.
@@ -85,10 +100,13 @@ public:
             tangentVectorAtParam = derivatives.col(1).matrix();
             curvatureAtParam     = derivatives.col(2).matrix();
 
-            double searchGradient  = tangentVectorAtParam.dot(vectorToParam);
-            double searchIncrement = curvatureAtParam.dot(vectorToParam) * pow(tangentVectorAtParam, 2.);
+            tangentVectorAtParam.normalize();
+            curvatureAtParam.normalize();
 
-            closestParam = previousClosestParam - searchGradient / searchIncrement;
+            double searchGradient  = tangentVectorAtParam.dot(vectorToParam);
+            double searchIncrement = curvatureAtParam.dot(vectorToParam) + std::pow(tangentVectorAtParam.norm(), 2.);
+
+            closestParam -= searchGradient / searchIncrement;
 
             // Constrain within the bounds of the spline, (1,0), C++ 17 provides std::clamp() to accomplish this.
             closestParam = std::max(std::min(closestParam, 1.), 0.);
@@ -96,17 +114,17 @@ public:
             // End the search if we are within our search threshold.
             if (((closestParam - previousClosestParam) * tangentVectorAtParam).norm() < searchThreshold)
             {
-                previousClosestParam = closestParam;
                 return closestParam;
             }
 
             counter++;
+            previousClosestParam = closestParam;
         }
 
         return -1;
-    }   
+    }
 
-    Eigen::Vector3d calculateSplineDerivatives(const double& parameterU, const int derivativeDegree)
+    Eigen::Vector3d calculateSplineDerivative(const double& parameterU, const int derivativeDegree)
     {
 
         if (derivativeDegree > SPLINE_DEGREE)
@@ -119,27 +137,45 @@ public:
     }
 
     /**
+     * \brief Returns the curvature on the spline at parameterU
+     * \parameterU The parameter of interest along the spline.
+     */
+    double calculateCurvature(const double& parameterU)
+    {
+        Eigen::Vector3d tangentVector = calculateSplineDerivative(parameterU, 1);
+        Eigen::Vector3d tangentNormalVector = calculateSplineDerivative(parameterU, 2);
+
+        // k = |y' x y"| / ||y'^3||
+        double curvature = tangentVector.cross(tangentNormalVector).norm() / pow(tangentVector.norm(), 3.0);
+        return curvature;
+    }
+
+    Eigen::Vector3d calculateCentripetalAccelVector(const double& parameterU)
+    {
+        Eigen::Vector3d tangentVector = calculateSplineDerivative(parameterU, 1);
+        Eigen::Vector3d tangentNormalVector = calculateSplineDerivative(parameterU, 2);
+
+        // Ensure perpendicularity to tangent vector.
+        Eigen::Vector3d centripetalAccelVector = tangentVector.cross(tangentNormalVector.cross(tangentVector));
+        return centripetalAccelVector;
+    }
+
+    /**
      * \brief Compute the maximum tangential speed at one point on the spline.
      * \param parameterU The point of interest on the spline.
      * \param curvature The computed curvature at that point on the spline.
      */
     double calculateMaxTangentialSpeed(const double& parameterU)
     {
-        tangentVector = calculateSplineDerivatives(parameterU, 1);
-        tangentNormalVector = calculateSplineDerivatives(parameterU, 2);
+        Eigen::Vector3d centripetalAccelVector = calculateCentripetalAccelVector(parameterU);
 
-        // Ensure perpendicularity to tangent vector.
-        centripetalAccelVector = tangentVector.cross(tangentNormalVector.cross(tangentVector));
-        
         double maxTangentSpeed = MAX_SPEED;
 
         if (centripetalAccelVector.norm() > 0)
         {
             centripetalAccelVector.normalize();
 
-            // k = |y' x y"| / ||y'^3||
-            curvature = tangentVector.cross(tangentNormalVector).norm()
-                               / pow(tangentVector.norm(), 3.0);
+            double curvature = calculateCurvature(parameterU);
 
             // a = v^2 / r   =>   v^2 * k   =>  v = sqrt(a / k)
             maxTangentSpeed = std::min(sqrt(MAX_ACCEL * curvature), MAX_SPEED);
@@ -162,7 +198,7 @@ public:
 
         if (inertialAccelVector.norm() == 0.)
         {
-            inertialAccelVector = Eigen::Vector3d(0., 0., -1.e-8); // A zero vector would result in an undefined orientation.
+            inertialAccelVector = Eigen::Vector3d(0., 0., 1.e-8); // A zero vector would result in an undefined orientation.
         }
 
         return inertialAccelVector;
@@ -176,7 +212,7 @@ private:
     Eigen::Vector3d tangentVector{Eigen::Vector3d::Zero()};
     Eigen::Vector3d tangentNormalVector{Eigen::Vector3d::Zero()};
     
-    Eigen::Vector3d gravityVector{Eigen::Vector3d(0., 0., -GRAVITY)};
+    Eigen::Vector3d gravityVector{GRAVITY* Vector3d::UnitZ()};
     
     double tangentAccelerationGain{1.}; // Proportional gain to tilt the trust vector.
     double previousClosestParam{0.};    // Previous closest spline parameter.
